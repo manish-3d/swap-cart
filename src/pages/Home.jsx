dda131513fd5536787687298cc1670fab52f9c99
manish-3d <manishkumar.intc@gmail.com>
2026-03-21 14:12:01 +0530

    error handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,31 @@
-import { Link } from "react-router";
-import products from "../data/products";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => {
+      if (res.status >= 400) {
+        throw new Error("Server error");  // 🔥 IMPORTANT
+      }
+      return res.json();
+    })
+    .then((data) => setProducts(data))
+    .catch((err) => setError(err));
+}, []);
+if (error) {
+  return <h2>❌ Failed to load products</h2>;
+}
   return (
     <div>
       <h1>🛒 Products</h1>
 
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.name}</h3>
-          <p>₹{product.price}</p>
-
-          {/* 🔥 LINK TO DYNAMIC ROUTE */}
-          <Link to={`/product/${product.id}`}>View Product</Link>
-
-          <hr />
+      {products.map((p) => (
+        <div key={p.id}>
+          <h3>{p.title}</h3>
+          <p>₹{p.price}</p>
         </div>
       ))}
     </div>
